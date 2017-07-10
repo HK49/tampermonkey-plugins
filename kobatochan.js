80432bfb4d3a33b288bbb0d6cf052ffd1cca708f
HK49 <timrja@gmail.com>
2017-07-10 16:57:02 +0300

    kobatochan 0.37 - expanding rules

--- kobatochan.js
+++ kobatochan.js
@@ -9,14 +9,14 @@
 // @require        https://rawgit.com/HK49/tampermonkey-plugins/master/libs/daynight.js
 // @require        https://rawgit.com/HK49/tampermonkey-plugins/master/libs/fontsize.js
 // @require        https://rawgit.com/HK49/tampermonkey-plugins/master/libs/fullscreen.js
-// @version        0.36
+// @version        0.37
 // @grant          GM_addStyle
 // @run-at         document-start
 // ==/UserScript==
 
 if(/prologue|chapter|i-am-the-monarch/.test(window.location.pathname)) {
   waitress('.content-sidebar-wrap', { width: '100% !important' });
-  waitress('#primary', { width: '33rem', float: 'none', margin: '0 auto', boxShadow: '0 0 3rem 0 hsla(0, 0%, 6%, .6)' });
+  waitress('#primary', { width: '33rem', minWidth: '50vw', maxWidth: '90vw', float: 'none', margin: '0 auto', boxShadow: '0 0 3rem 0 hsla(0, 0%, 6%, .6)' });
   waitress('#secondary, #third-sidebar, #wpadminbar, #header-image', { display: 'none' });
   waitress('html', { marginTop: '0 !important' });
   waitress('p', { fontSize: '1rem', lineHeight: '1.3rem', marginBottom: '1.8rem' });
@@ -25,9 +25,9 @@ if(/prologue|chapter|i-am-the-monarch/.test(window.location.pathname)) {
   dayNight((function() {
     waitress('article[id^=post], #primary', { backgroundColor: 'rgb(223, 200, 178) !important' });
     waitress('article[id^=post], p, span, h1, strong, time', { color: 'rgb(59, 36, 30) !important' });
-    waitress('#main, .content-sidebar-wrap, #branding, .wrapper, body, html', { backgroundColor: 'rgb(148, 108, 71)' });
-    waitress('#access', { background: 'rgb(148, 108, 71)' });
-    waitress('body, btn, #night_btn, #btn_full', { color: 'rgb(243, 181, 157) !important' });
+    waitress('#main, .content-sidebar-wrap, #branding, .wrapper, body, html', { backgroundColor: 'rgb(195, 147, 104)' });
+    waitress('#access', { background: 'rgb(195, 147, 104)' });
+    waitress('body, btn, #night_btn, #btn_full', { color: 'rgb(246, 200, 182) !important' });
     waitress('a, a[href*=kobato]', { color: 'rgb(146, 89, 79) !important' });
     waitress('a:hover, a[href*=kobato]:hover', { color: 'rgb(180, 130, 100) !important' });
   }), (function() {
