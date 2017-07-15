f118033cfc9b4ea1a882be27aa6c8e1e739193aa
HK49 <timrja@gmail.com>
2017-07-15 23:10:47 +0300

    kobatochan navbar fix listing; enclosed in anons waitress.style calls; sorted funcs

--- kobatochan.js
+++ kobatochan.js
@@ -58,163 +58,6 @@ waitress('#dark-css', () => document.getElementById('dark-css').remove());
 waitress('#text-21', () => document.getElementById('text-21').remove());
 
 
-// input
-(() => {
-  css.input = {};
-  const input = {
-    i: [
-      'input',
-      '#page input',
-      '#page textarea',
-      '#page #respond input',
-      '#page #respond #submit',
-      '#page #comments textarea',
-      '#branding #searchform > input#s'
-    ],
-    form: 'form',
-    ph: '::placeholder',
-    phH: ':hover::placeholder'
-  };
-  input.focus = input.i.map((i) => i + ':focus');
-  input.hover = input.i.map((i) => i + ':hover:not(:focus)');
-
-  css.input[input.i] = Object.assign({
-    background: dark.tint(0.2),
-    border: '1px solid ' + dark.tint(),
-    borderRadius: '4px',
-    boxShadow: 'none',
-    font: 'normal 0.8rem/1rem Arial, "sans-serif"',
-    padding: '.5rem',
-    textShadow: 'none',
-    transition: 'background-color .4s ease'
-  }, fix.color('!'));
-  css.input[input.focus] = { background: dark.tint(0.4) };
-  css.input[input.hover] = { background: light.tint(0.1) };
-  css.input[input.form] = { overflow: 'hidden' };
-  css.input[input.ph] = Object.assign(
-    { transition: 'transform .8s ease-in-out' },
-    fix.color()
-  );
-  css.input[input.phH] = { transform: 'translateX(200%)' };
-
-  waitress(['input', 'textarea'], waitress.style(css.input, false, 'input_style'));
-})();
-
-
-// navbar
-(() => {
-  const nav = {};
-  css.nav = {};
-  nav.head = '#header-menu';
-  nav.root = nav.head + ' > nav';
-  nav.wrap = nav.root + ' > .wrapper';
-  nav.menu = nav.wrap + ' > .menu';
-  nav.item = nav.menu + ' .menu-item';
-  nav.link = nav.item + ' > a';
-  nav.subm = nav.link + ' + .sub-menu';
-  nav.subl = nav.subm + ' > li';
-  nav.subs = nav.subl + ' > a';
-  nav.submsubm = nav.subs + ' + .sub-menu';
-  nav.hoverLink = nav.item + ':hover' + ' > .sub-menu';
-  nav.hoverSubm = nav.subm + ':hover';
-  nav.hoverSubs = nav.subm + ' > li:hover::before';
-
-  css.nav[nav.wrap] = { width: '100%' };
-  css.nav[nav.menu] = {
-    display: 'inline-flex',
-    flexFlow: 'row wrap',
-    width: '100%'
-  };
-  css.nav[nav.item] = {
-    display: 'flex',
-    flex: '1 0 auto'
-  };
-  css.nav[nav.link] = {
-    fontSize: '.7rem',
-    lineHeight: '1.65rem',
-    margin: '0 auto',
-    padding: '0 .3rem',
-    width: 'max-content'
-  };
-  css.nav[nav.subm] = {
-    boxShadow: 'none',
-    display: 'flex',
-    position: 'absolute',
-    flexFlow: 'column nowrap',
-    left: '.1rem',
-    maxHeight: '0',
-    maxWidth: '12rem',
-    opacity: '0',
-    overflow: 'hidden',
-    top: 'calc(1.65rem * 2)',
-    transition: 'all .5s ease',
-    width: 'auto'
-  };
-  css.nav[[nav.hoverLink, nav.hoverSubm]] = {
-    maxHeight: 1e4 + 'px !important',
-    overflow: 'visible',
-    top: '1.65rem'
-  };
-  css.nav[nav.hoverLink] = { opacity: '.7' };
-  css.nav[nav.hoverSubm] = { opacity: '1' };
-  css.nav[nav.subl] = {
-    borderLeft: '.2rem solid',
-    zIndex: '1'
-  };
-  css.nav[nav.subl + '::after'] = {
-    borderColor: 'inherit',
-    borderTop: '.1rem solid',
-    bottom: '0',
-    content: '\'\'',
-    left: '-.2rem',
-    position: 'absolute',
-    transition: 'width .5s 1s ease-out',
-    width: '0'
-  };
-  css.nav[
-    [nav.hoverLink, nav.hoverSubm].map((e) => e + ' > li::after')
-  ] = { width: 'calc(100% + .2rem)' };
-  css.nav[nav.subs] = {
-    borderBottom: 'none',
-    fontSize: '.6rem',
-    lineHeight: '1.6rem',
-    margin: '0'
-  };
-  css.nav[nav.hoverSubs] = {
-    background: dark.tint(0.8),
-    content: '\'\'',
-    height: '100%',
-    position: 'absolute',
-    top: '0',
-    width: '100%',
-    zIndex: '-1'
-  };
-  css.nav[nav.subm + '::before'] = {
-    borderLeft: '.2rem solid',
-    borderColor: 'inherit',
-    content: '\'\'',
-    height: '1.65rem',
-    position: 'absolute',
-    top: '0',
-    transition: 'all .5s .5s ease'
-  };
-  css.nav[[nav.hoverLink, nav.hoverSubm].map((e) => e + '::before')] = { top: '-1.65rem' };
-  css.nav[nav.submsubm] = { top: 'calc(1.6rem * 3)', zIndex: '-1' };
-  css.nav[
-    [
-      [nav.subl, nav.submsubm].map((e) => e + ':hover'),
-      nav.subl + ':hover > a + .submenu'
-    ].join(',')
-  ] = { zIndex: '2' };
-  css.nav[
-    [nav.submsubm + ':hover', nav.subl + ':hover > a + .sub-menu'].join(',')
-  ] = { top: '1.6rem' };
-  css.nav[nav.submsubm + ' > li > a'] = { lineHeight: '1.6rem' };
-
-  waitress('nav', waitress.style(css.nav, false, 'navbar_style'));
-})();
-
-
 //fix for social buttons
 waitress(['i', 'ss'], (iss = {}) => {
   ['i', 'ss'].forEach((e) => { iss[e] = document.getElementsByTagName(e); });
@@ -228,104 +71,11 @@ waitress(['i', 'ss'], (iss = {}) => {
 
 //globals
 //waitress('#page', waitress.style({ '*, a, #page *, #page a': fix.bgandcolor() }));
-waitress('#page', waitress.style({ '*, a, #page *, #page a': fix.color() }));
-
-if(!(/prologue|chapter/).test(window.location.pathname)) {
-  (() => {
-    // homepage
-    css.home = {};
-    nodes.home = {
-      content: '.home #content',
-      article: '.home #content article'
-    };
-    css.home[nodes.home.content] = { borderColor: 'inherit' };
-    css.home[nodes.home.article] = {
-      borderBottom: '0.2rem solid',
-      borderColor: 'inherit',
-      borderRadius: '0'
-    };
-    waitress.style(css.home, false, 'homepage_style');
-
-
-    // wrapper and sidebars. (sidebars are not shown when reading)
-    css.wrap = {};
-    nodes.wrap = {
-      wrapper: '#main-wrapper > #main > .wrapper',
-      content: '#main-wrapper > #main > .wrapper > .content-sidebar-wrap',
-      primary: '#primary',
-      secondary: '#secondary',
-      sidebar: '#third-sidebar',
-      widgets: [
-        '.widget',
-        '.widget > .widget-title',
-        '.widget > ul',
-        '.widget > ul > li'
-      ]
-    };
-    css.wrap[nodes.wrap.wrapper] = {
-      display: 'flex',
-      flexFlow: 'row-reverse wrap',
-      color: 'inherit !important',
-      width: '100%'
-    };
-    css.wrap[nodes.wrap.content] = {
-      flex: '1 0 calc(33rem + 340px)',
-      display: 'flex',
-      flexFlow: 'row wrap'
-    };
-    css.wrap[nodes.wrap.primary] = { flex: '0 0 33rem' };
-    css.wrap[nodes.wrap.secondary] = {
-      flex: '0 1 auto',
-      color: 'inherit !important'
-    };
-    css.wrap[nodes.wrap.sidebar] = {
-      display: 'block',
-      flex: '0 1 auto',
-      color: 'inherit !important'
-    };
-    css.wrap[nodes.wrap.widgets] = fix.bgandcolor('!', '!');
-
-    waitress.style(css.wrap, false, 'main_layout_style');
-  })();
-}
-
-
-// comments
-(() => {
-  css.comments = {};
-  nodes.comments = {
-    main: [
-      '#comments',
-      '#comments > .commentlist',
-      '#comments > .commentlist > .comment',
-      'article[id^=\'comment\']',
-      'article[id^=\'comment\'] + .children',
-      '#page #comments-title'
-    ],
-    tinted: [
-      'article[id^=\'comment\'] + .children > .comment',
-      '#author-info'
-    ],
-    comment: '#page #comments .comment article',
-    orFB: '#respond li[id^=theChampTabs]'
-  };
-  css.comments[nodes.comments.main] = fix.bgandcolor('!', '!');
-  css.comments[nodes.comments.tinted] = {
-    background: dark.tint(0.3) + ' !important',
-    color: 'inherit !important'
-  };
-  css.comments[nodes.comments.comment] = {
-    borderBottom: '4px solid ' + dark.tint(),
-    borderRadius: '4px'
-  };
-  css.comments[nodes.comments.orFB] = fix.bgandcolor('!', '!');
-
-  waitress('#comments', waitress.style(css.comments, false, 'comments_style'));
-})();
+waitress('#page', () => waitress.style({ '*, a, #page *, #page a': fix.color() }));
 
 
 // author/tl notes
-waitress('.footnotes', waitress.style({
+waitress('.footnotes', () => waitress.style({
   '#page .footnotes, #page .footnotes ol, #page .footnotes li': Object.assign({
     fontSize: '0.8rem',
     lineHeight: '1.2rem'
@@ -476,3 +226,317 @@ scaleFont();
 
 // btn from fullscreen.js
 fullScreen();
+
+
+// input
+(() => {
+  css.input = {};
+  const input = {
+    i: [
+      'input',
+      '#page input',
+      '#page textarea',
+      '#page #respond input',
+      '#page #respond #submit',
+      '#page #comments textarea',
+      '#branding #searchform > input#s'
+    ],
+    form: 'form',
+    ph: '::placeholder',
+    phH: ':hover::placeholder'
+  };
+  input.focus = input.i.map((i) => i + ':focus');
+  input.hover = input.i.map((i) => i + ':hover:not(:focus)');
+
+  css.input[input.i] = Object.assign({
+    background: dark.tint(0.2),
+    border: '1px solid ' + dark.tint(),
+    borderRadius: '4px',
+    boxShadow: 'none',
+    font: 'normal 0.8rem/1rem Arial, "sans-serif"',
+    padding: '.5rem',
+    textShadow: 'none',
+    transition: 'background-color .4s ease'
+  }, fix.color('!'));
+  css.input[input.focus] = { background: dark.tint(0.4) };
+  css.input[input.hover] = { background: light.tint(0.1) };
+  css.input[input.form] = { overflow: 'hidden' };
+  css.input[input.ph] = Object.assign(
+    { transition: 'transform .8s ease-in-out' },
+    fix.color()
+  );
+  css.input[input.phH] = { transform: 'translateX(200%)' };
+
+  waitress(['input', 'textarea'], () => waitress.style(css.input, false, 'input_style'));
+})();
+
+
+// navbar
+(() => {
+  const nav = {};
+  css.nav = {};
+  nav.head = '#header-menu';
+  nav.root = nav.head + ' > nav';
+  nav.wrap = nav.root + ' > .wrapper';
+  nav.menu = nav.wrap + ' > .menu';
+  nav.item = nav.menu + ' > .menu-item';
+  nav.link = nav.item + ' > a';
+  nav.subm = nav.link + ' + .sub-menu';
+  nav.subi = nav.subm + ' > li';
+  nav.subl = nav.subi + ' > a';
+  nav.submsubm = nav.subl + ' + .sub-menu';
+  nav.hoverLink = nav.item + ':hover' + ' > .sub-menu';
+  nav.hoverSubm = nav.subm + ':hover';
+  nav.hoverSubs = nav.subm + ' > li:hover::before';
+
+  css.nav[nav.wrap] = { width: '100%' };
+  css.nav[nav.menu] = {
+    display: 'inline-flex',
+    flexFlow: 'row wrap',
+    width: '100%'
+  };
+  css.nav[nav.item] = {
+    display: 'flex',
+    flex: '1 0 auto'
+  };
+  css.nav[nav.link] = {
+    fontSize: '.7rem',
+    lineHeight: '1.65rem',
+    margin: '0 auto',
+    padding: '0 .3rem',
+    width: 'max-content'
+  };
+  css.nav[nav.subm] = {
+    boxShadow: 'none',
+    display: 'flex',
+    position: 'absolute',
+    flexFlow: 'column nowrap',
+    left: '.1rem',
+    maxHeight: '0',
+    minWidth: 'max-content',
+    opacity: '0',
+    overflow: 'hidden',
+    top: 'calc(1.65rem * 2)',
+    transition: 'all .5s ease'
+  };
+  css.nav[[nav.hoverLink, nav.hoverSubm]] = {
+    maxHeight: 1e4 + 'px !important',
+    overflow: 'visible',
+    top: '1.65rem'
+  };
+  css.nav[nav.hoverLink] = { opacity: '.7' };
+  css.nav[nav.hoverSubm] = { opacity: '1' };
+  css.nav[nav.subi] = {
+    borderLeft: '.2rem solid',
+    zIndex: '1'
+  };
+  css.nav[nav.subi + '::after'] = {
+    borderColor: 'inherit',
+    borderTop: '.1rem solid',
+    bottom: '0',
+    content: '\'\'',
+    left: '-.2rem',
+    position: 'absolute',
+    transition: 'width .5s 1s ease-out',
+    width: '0'
+  };
+  css.nav[
+    [nav.hoverLink, nav.hoverSubm].map((e) => e + ' > li::after')
+  ] = { width: 'calc(100% + .2rem)' };
+  css.nav[nav.subl] = {
+    borderBottom: 'none',
+    fontSize: '.6rem',
+    lineHeight: '1.6rem',
+    margin: '0',
+    padding: '0 .3rem',
+    width: '100%'
+  };
+  css.nav[nav.hoverSubs] = {
+    background: dark.tint(0.8),
+    content: '\'\'',
+    height: '100%',
+    position: 'absolute',
+    top: '0',
+    width: '100%',
+    zIndex: '-1'
+  };
+  css.nav[nav.subm + '::before'] = {
+    borderLeft: '.2rem solid',
+    borderColor: 'inherit',
+    content: '\'\'',
+    height: '1.65rem',
+    position: 'absolute',
+    top: '0',
+    transition: 'all .5s .5s ease'
+  };
+  css.nav[[nav.hoverLink, nav.hoverSubm].map((e) => e + '::before')] = { top: '-1.65rem' };
+  css.nav[nav.submsubm] = { top: 'calc(1.6rem * 3)', zIndex: '-1' };
+  css.nav[
+    [
+      [nav.subi, nav.submsubm].map((e) => e + ':hover'),
+      nav.subi + ':hover > a + .submenu'
+    ].join(',')
+  ] = { zIndex: '2' };
+  css.nav[
+    [nav.submsubm + ':hover', nav.subi + ':hover > a + .sub-menu'].join(',')
+  ] = { top: '1.6rem' };
+  css.nav[nav.submsubm + ' > li > a'] = { lineHeight: '1.6rem' };
+
+  waitress('nav', () => waitress.style(css.nav, false, 'navbar_style'));
+})();
+
+
+if(!(/prologue|chapter/).test(window.location.pathname)) {
+  (() => {
+    // homepage
+    css.home = {};
+    nodes.home = {
+      content: '.home #content',
+      article: '.home #content article'
+    };
+    css.home[nodes.home.content] = { borderColor: 'inherit' };
+    css.home[nodes.home.article] = {
+      borderBottom: '0.2rem solid',
+      borderColor: 'inherit',
+      borderRadius: '0'
+    };
+    waitress.style(css.home, false, 'homepage_style');
+
+
+    // wrapper and sidebars. (sidebars are not shown when reading)
+    css.wrap = {};
+    nodes.wrap = {
+      wrapper: '#main-wrapper > #main > .wrapper',
+      content: '#main-wrapper > #main > .wrapper > .content-sidebar-wrap',
+      primary: '#primary',
+      secondary: '#secondary',
+      sidebar: '#third-sidebar',
+      widgets: [
+        '.widget',
+        '.widget > .widget-title',
+        '.widget > ul',
+        '.widget > ul > li'
+      ]
+    };
+    css.wrap[nodes.wrap.wrapper] = {
+      display: 'flex',
+      flexFlow: 'row-reverse wrap',
+      color: 'inherit !important',
+      width: '100%'
+    };
+    css.wrap[nodes.wrap.content] = {
+      display: 'flex',
+      flex: '1 0 35rem',
+      flexFlow: 'row wrap',
+      margin: '0 auto',
+      maxWidth: '90vw',
+      minWidth: '80vw',
+      position: 'relative'
+    };
+    css.wrap[nodes.wrap.primary] = {
+      flex: '1 1 auto',
+      margin: '0 auto',
+      maxWidth: '90vw',
+      minWidth: '60vw',
+      position: 'relative'
+    };
+    css.wrap[nodes.wrap.secondary] = {
+      color: 'inherit !important',
+      flex: '1 1 auto',
+      maxWidth: '20vw',
+      minWidth: '12rem'
+    };
+    css.wrap[nodes.wrap.sidebar] = {
+      color: 'inherit !important',
+      display: 'block',
+      flex: '0 1 auto',
+      maxWidth: '17rem',
+      minWidth: '8rem'
+    };
+    css.wrap[nodes.wrap.widgets] = fix.bgandcolor('!', '!');
+
+    waitress.style(css.wrap, false, 'main_layout_style');
+  })();
+}
+
+
+// comments
+(() => {
+  css.comments = {};
+  nodes.comments = {
+    main: [
+      '#comments',
+      '#comments > .commentlist',
+      '#comments > .commentlist > .comment',
+      'article[id^=\'comment\']',
+      'article[id^=\'comment\'] + .children',
+      '#page #comments-title'
+    ],
+    tinted: [
+      'article[id^=\'comment\'] + .children > .comment',
+      '#author-info'
+    ],
+    comment: '#page #comments .comment article',
+    orFB: '#respond li[id^=theChampTabs]'
+  };
+  css.comments[nodes.comments.main] = fix.bgandcolor('!', '!');
+  css.comments[nodes.comments.tinted] = {
+    background: dark.tint(0.3) + ' !important',
+    color: 'inherit !important'
+  };
+  css.comments[nodes.comments.comment] = {
+    borderBottom: '4px solid ' + dark.tint(),
+    borderRadius: '4px'
+  };
+  css.comments[nodes.comments.orFB] = fix.bgandcolor('!', '!');
+
+  waitress('#comments', () => waitress.style(css.comments, false, 'comments_style'));
+})();
+
+
+// cookie law
+(() => {
+  const law = {};
+  css.law = {};
+  law.bar = '#main #third-sidebar';
+  law.widget = law.bar + ' > #eu_cookie_law_widget-2';
+  law.inside = law.widget + ' > #eu-cookie-law';
+  law.link = law.insid + ' > a';
+  law.submit = law.inside + ' > form > .accept';
+
+  css.law[law.widget] = { maxWidth: '36vw' };
+  css.law[law.inside] = {
+    font: 'normal .5rem/.75rem Helvetica, sans-serif',
+    padding: '.5rem .5rem calc(2.25rem + 1px)',
+    textAlign: 'justify'
+  };
+  css.law[law.link] = {
+    display: 'inline-block',
+    textAlign: 'center',
+    width: '100%'
+  };
+  css.law[law.submit] = {
+    border: '1px solid',
+    borderColor: 'inherit',
+    borderRadius: '4px',
+    bottom: '.5rem',
+    left: '50%',
+    margin: '0',
+    padding: '.25rem .5rem',
+    position: 'absolute',
+    right: 'initial',
+    transform: 'translateX(-50%)',
+    width: '80%'
+  };
+
+  [css.law[law.submit], css.law[law.link]].forEach((e) => {
+    Object.assign(e, { font: 'normal .5rem/.75rem Helvetica, sans-serif' });
+  });
+  Object.keys(css.law).forEach((e) => {
+    Object.assign(e, { backgroundColor: 'inherit !important' });
+  });
+
+  waitress('#eu_cookie_law_widget-2',
+    () => waitress.style(css.law, false, 'eu_cookie_law_widget_style')
+  );
+})();
