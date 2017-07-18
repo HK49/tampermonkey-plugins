8fa33ab7e99f8c9076e847a962d7de1e28b62e5e
HK49 <timrja@gmail.com>
2017-07-18 09:50:26 +0300

    fullscreen.js cleanup; promises; return null if btn already exists; fetch waitress(.style) if not available.

--- libs/fullscreen.js
+++ libs/fullscreen.js
@@ -1,108 +1,109 @@
-/* global waitress */
+async function fullScreen() {
+  if (!document.body) { return window.requestAnimationFrame(fullScreen); }
 
-function fullScreen() {
-  if(document.readyState !== "complete") {
-    window.requestAnimationFrame(fullScreen);
-  } else {
-    var setAttributes = function(el, attrs) {
-      Object.keys(attrs).forEach(function(i) { el.setAttribute(i, attrs[i]); });
-    };
+  const btn = document.createElement("btn");
 
-    var btnInit = function() {
-      var screenFull, req;
+  const attrs = { id: "fullscreen_btn", title: "Enter full screen mode" };
+  (() => Object.keys(attrs).forEach(i => btn.setAttribute(i, attrs[i])))();
 
-      var btn = document.getElementById("btn_full") || document.createElement("btn");
-      setAttributes(btn, { id: "btn_full", tytle: "Enter full screen mode" });
-      Object.assign(btn.style, {
-        position: 'fixed',
-        bottom: '10px',
-        left: '10px',
-        width: '26px',
-        height: '26px',
+  if (document.getElementById(attrs.id)) { return null; }
+
+  let full = false;
+  let reqF = null;
+
+  const chain = await new Promise((resolve) => {
+    if (waitress) { resolve(waitress); }
+  }).catch(
+    fetch('https://rawgit.com/HK49/tampermonkey-plugins/master/libs/waitress.js')
+    .then(raw => raw.text())
+    .then(eval)
+  ).then((styleress) => {
+    styleress.style({
+      "body:-webkit-full-screen": {
+        backgroundColor: window.getComputedStyle(document.body).backgroundColor,
+        height: "100%",
+        overflowY: "scroll",
+        width: "100%",
+      },
+      [`#${attrs.id}`]: {
         border: 'solid 2px',
+        bottom: '10px',
         cursor: 'pointer',
+        height: '26px',
+        left: '10px',
         opacity: '.6',
+        position: 'fixed',
         transition: 'all .6s ease',
-        zIndex: String(1e+5)
-      });
-      if(!document.getElementById("btn_full")) {
-        document.body.insertBefore(btn, document.body.firstElementChild);
-      }
-
-      document.getElementsByTagName("html")[0].style.backgroundColor = document.body.style.background;
-      var bodyStyle = document.createElement("style");
-      bodyStyle.setAttribute("name", "fullscreen");
-      bodyStyle.innerHTML = [
-        "body:-webkit-full-screen",
-        "{",
-        "overflow-y: scroll;",
-        "background-color: " + document.body.style.background + ";",
-        "width: 100%;",
-        "height: 100%",
-        "}",
-        "#btn_full:hover { opacity: 1 !important }"
-      ].join(" ");
-      var sTags = document.head.getElementsByTagName('style');
-      document.head.insertBefore(bodyStyle, (sTags ? sTags[sTags.length - 1].nextSibling : document.head.lastElementChild));
-
-      waitress("#btn_full::before, #btn_full::after", {
-        content: '\'\'',
-        width: '0',
-        height: '0',
-        position: 'absolute',
-        borderTopColor: 'transparent',
+        width: '26px',
+        zIndex: `${1e+5}`,
+      },
+      [`#${attrs.id}:hover`]: { opacity: '1' },
+      [`#${attrs.id}::before, #${attrs.id}::after`]: {
         borderBottomColor: 'transparent',
         borderStyle: 'solid',
-        transition: "all .3s ease"
-      });
-      // waitress here is unneeded overkill, but just for fun it's ok
+        borderTopColor: 'transparent',
+        content: '\'\'',
+        height: '0',
+        position: 'absolute',
+        transition: "all .3s ease",
+        width: '0',
+      },
+    }, false, 'fullscreen_btn_style');
 
-      var restyle = function(full) {
-        btn.setAttribute("title", (screenFull ? "Exit" : "Enter") + " full screen mode");
-        waitress("#btn_full::before", {
-          left: (full ? '0' : '3px'),
-          bottom: (full ? '0' : '3px'),
-          borderWidth: '10px 0 0 10px'
-        });
-        waitress("#btn_full::after", {
-          right: (full ? '0' : '3px'),
-          top: (full ? '0' : '3px'),
-          borderWidth: '0 10px 10px 0'
-        });
-        waitress("#btn_full:hover::before", { left: (!full ? '0' : '3px'), bottom: (!full ? '0' : '3px') });
-        waitress("#btn_full:hover::after", { right: (!full ? '0' : '3px'), top: (!full ? '0' : '3px') });
-      };
+    document.body.insertBefore(btn, document.body.firstElementChild);
 
-      restyle(screenFull);
-
-      var action = function(o) {
-        if(!screenFull) {
-          req = o.requestFullScreen || o.webkitRequestFullscreen || o.mozRequestFullScreen || o.msRequestFullscreen;
-          screenFull = true;
-          req.apply(o);
-        } else {
-          req = document.exitFullscreen || document.webkitExitFullscreen || document.mozExitFullScreen || document.msExitFullscreen;
-          screenFull = false;
-          req.bind(document).apply();
-        }
-        restyle(screenFull);
-      };
-
-      btn.addEventListener('click', function(){ action(document.body); });
-    };
-
-    if(!waitress) {
-      var tag = document.createElement('script');
-      setAttributes(tag, {
-        type: "text/javascript",
-        async: '',
-        src: "https://cdn.rawgit.com/HK49/tampermonkey-plugins/stable/libs/waitress.js"
-      });
-      // listener before insertion
-      tag.addEventListener("load", function() { btnInit(); });
-      document.head.insertBefore(tag, (document.head.getElementsByTagName('script') ? document.head.getElementsByTagName('script')[document.head.getElementsByTagName('script').length - 1].nextSibling : document.head.lastElementChild));
-    } else {
-      btnInit();
+    return styleress;
+  }).catch(window.console.error)
+  .then((styleress) => {
+    function restyle(f) {
+      // change style according to screen: full or not
+      btn.setAttribute("title", `${f ? "Exit" : "Enter"} full screen mode`);
+      styleress.style({
+        [`#${attrs.id}::before`]: {
+          borderWidth: '10px 0 0 10px',
+          bottom: (f ? '0' : '3px'),
+          left: (f ? '0' : '3px'),
+        },
+        [`#${attrs.id}::after`]: {
+          borderWidth: '0 10px 10px 0',
+          right: (f ? '0' : '3px'),
+          top: (f ? '0' : '3px'),
+        },
+        [`#${attrs.id}:hover::before`]: {
+          bottom: (!f ? '0' : '3px'),
+          left: (!f ? '0' : '3px'),
+        },
+        [`#${attrs.id}:hover::after`]: {
+          right: (!f ? '0' : '3px'),
+          top: (!f ? '0' : '3px'),
+        },
+      }, false, 'fullscreen_btn_style');
     }
-  }
+
+    restyle(full);
+
+    function action(o) {
+      if (!full) {
+        reqF = o.requestFullScreen
+        || o.webkitRequestFullscreen
+        || o.mozRequestFullScreen
+        || o.msRequestFullscreen;
+        full = true;
+        reqF.apply(o);
+      } else {
+        reqF = document.exitFullscreen
+        || document.webkitExitFullscreen
+        || document.mozExitFullScreen
+        || document.msExitFullscreen;
+        full = false;
+        reqF.bind(document).apply();
+      }
+      restyle(full);
+    }
+
+    btn.addEventListener('click', () => action(document.body));
+  });
 }
+
+// init
+fullScreen();
