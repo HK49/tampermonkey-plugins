1142688de32cee61eb7abfbb422c620cba1a0b1d
HK49 <timrja@gmail.com>
2017-07-28 20:37:15 +0300

    volare: color scheme, scrollbar, daynight considers forementioned scheme

--- volare.js
+++ volare.js
@@ -15,6 +15,14 @@
 // ==/UserScript==
 
 
+// color scheme
+const scheme = {
+  first: l => `hsl(28, 50%, ${l}%)`,
+  second: l => `hsl(14, 50%, ${l}%)`,
+  third: l => `hsl(14, 36%, ${l}%)`,
+};
+
+
 // disqus listen to msg and change according to day/dark themes
 if (window.location.host === `disqus.com`) {
   window.addEventListener("message", (e) => {
@@ -37,6 +45,24 @@ if (window.location.host === `disqus.com`) {
     .then(eval)
     .catch(window.console.error);
 })();
+
+
+// style scrollbar
+waitress('html', () => {
+  waitress.style({
+    'html::-webkit-scrollbar': {
+      width: '4px',
+    },
+    'html::-webkit-scrollbar-track': {
+      background: 'hsla(14, 43%, 56%, .4)',
+      zIndex: `${1e4}`,
+    },
+    'html::-webkit-scrollbar-thumb': {
+      background: 'hsla(14, 43%, 56%, .8)',
+      zIndex: `${1e4 + 1}`,
+    },
+  });
+});
 
 
 // sidebar object
@@ -75,7 +101,6 @@ waitress(`.${sb.class}`, () => {
     width: '340px',
     willChange: 'right',
   }, tr);
-  // TODO: what is parent node height smaller then sb height?
 
   waitress.style({
     [`#${sb.id} > :nth-child(n):not(#bar-bg):not(#bar-btn)`]: {
@@ -180,9 +205,6 @@ waitress('#menu-main-menu', () => waitress.style({
   },
 }));
 
-// promotion message can now have more width, that we removed custom css btn
-waitress('#text-45', { maxWidth: '80%' });
-
 // site wrapper
 waitress('#page', Object.assign({
   maxWidth: '90vw',
@@ -211,6 +233,11 @@ waitress('#content', {
   waitress(`#${e}`, () => document.getElementById(e).remove());
 });
 
+// promotion message
+waitress('#promotion-message', { border: '1px solid' });
+// promotion message can now have more width, that we removed custom css btn
+waitress('#text-45', { maxWidth: '80%' });
+
 
 // disqus wrapper
 waitress('#disqus_thread', {
@@ -223,61 +250,83 @@ waitress('#disqus_thread', {
 function lights(daytime) {
   const on = (daytime === "day");
 
-  // body, disqus wrapper
+  // body, disqus wrapper background
   waitress([
     'body',
     '#disqus_thread',
   ], {
-    background: `${on ? '' : 'rgb(117, 72, 55)'}`,
+    background: `${on ? scheme.first(66) : scheme.third(33)}`,
   });
 
   // navbar
   waitress('.nav-primary', () => {
     const nav = document.getElementsByClassName('nav-primary')[0];
-    nav.style.backgroundColor = `${on ? '' : 'hsl(14, 43%, 56%)'}`;
+    nav.style.backgroundColor = `${on ? scheme.second(55) : scheme.third(52)}`;
 
     waitress.style({
       [[
         '.nav-primary ul.menu .sub-menu a',
         '.nav-primary ul.menu .children a',
       ].join(', ')]: {
-        backgroundColor: `${on ? '' : 'hsl(14, 43%, 56%)'}`,
+        backgroundColor: `${on ? scheme.second(55) : scheme.third(52)}`,
       },
     });
   });
 
 
-  // promotion message
-  waitress('#promotion-message', {
-    border: '1px solid',
-    borderColor: `${on ? '' : 'rgb(196, 120, 98)'}`,
+  // borders color
+  waitress([
+    '#promotion-message',
+    '#content',
+    `#bar-bg`,
+    `#bar-btn`,
+    `#bar-btn::before`,
+    '#disqus_thread',
+  ], {
+    borderColor: `${on ? scheme.second(55) : scheme.third(52)}`,
   });
 
 
-  // background
-  waitress(['#content', '#promotion-message', `.${sb.class}`, '#bar-bg', '#bar-btn'], {
-    backgroundColor: `${on ? '' : 'rgb(59, 36, 30)'}`,
+  // page background
+  waitress([
+    '#content',
+    '#promotion-message',
+    `.${sb.class}`,
+    '#bar-bg',
+    '#bar-btn',
+  ], {
+    backgroundColor: `${on ? scheme.first(75) : scheme.third(16)}`,
   });
 
 
-  // text
+  // page font color
+  waitress([
+    '#content',
+    `.${sb.class}`,
+  ].concat(Array(6).fill('h').map((e, i) => e + (i + 1))), {
+    color: `${on ? scheme.first(22) : scheme.second(58)}`,
+  });
+
+  waitress.style({
+    '#main .entry-title': {
+      color: `${on ? scheme.first(22) : scheme.second(58)} !important`,
+    },
+    hr: {
+      backgroundColor: `${on ? scheme.first(22) : scheme.second(58)}`,
+    },
+  }, false, 'font_color_style');
+
+
+  // link colors
   waitress([
     'body',
-    'p',
-    '.sidebar-primary',
-  ].concat(Array(6).fill('h').map((e, i) => e + (i + 1))), {
-    color: `${on ? '' : 'rgb(196, 120, 98)'}`,
-  }, () => {
-    waitress('.entry-title', () => {
-      waitress.style({
-        '#main .entry-title': {
-          color: `${on ? '' : 'rgb(196, 120, 98)'} !important`,
-        },
-        hr: {
-          backgroundColor: `${on ? '' : 'rgb(196, 120, 98)'}`,
-        },
-      }, false, 'font_color_style');
-    });
+    'a',
+  ], {
+    color: `${on ? scheme.second(52) : scheme.second(48)}`,
+  });
+
+  waitress('a:hover', {
+    color: `${on ? scheme.second(62) : scheme.second(38)}`,
   });
 
 
