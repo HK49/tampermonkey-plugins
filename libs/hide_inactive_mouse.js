4d703cee4ce8cf613a2f21f3a009ce40efc2f939
HK49 <timrja@gmail.com>
2017-07-29 12:02:36 +0300

    hide_inactive_mouse update considering relevant(old indeed) webkit fixes

--- libs/hide_inactive_mouse.js
+++ libs/hide_inactive_mouse.js
@@ -1,41 +1,16 @@
-var mice = function() {
-  mice.hidden = mice.hidden || false;
-  // should be mentioned (bug: no mouse update without movement):
-  // https://bugs.chromium.org/p/chromium/issues/detail?id=26723 //and ?id=676644
-  return {
-    hide: function() {
-      document.addEventListener('mousemove', function(e) {
-        var passive = function() {
-          window.clearTimeout(mice.reveal);
-          document.body.style.cursor = "url(data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7), none";
-          // uri displays 1x1 px black dot in chrome
-          mice.hidden = true;
-          mice.reveal = window.setTimeout(function() {
-            mice.position = { x: e.clientX, y: e.clientY };
-            // storing position - if small movement don't reveal mouse cursor
-            mice.hidden = false;
-          }, 400);
-        };
-
-        var active = function() {
-          window.clearTimeout(mice.active);
-          document.body.style.cursor = "default";
-          mice.active = window.setTimeout(passive, 1e3);
-        };
-
-        if(mice.position && !mice.hidden) {
-          var moving = [
-            Math.abs(e.clientX - mice.position.x) > 75,
-            Math.abs(e.clientY - mice.position.y) > 75
-          ];
-          if(moving.includes(true)) { active(); }
-        } else if(!mice.position && !mice.hidden) {
-          active();
-        }
-      });
-    }
-  };
+const mouse = {
+  timer: (t = 2e3) => setTimeout(
+    () => (document.documentElement.style.cursor = 'none'),
+    t,
+  ),
+  hide: (t) => {
+    document.addEventListener('mousemove', () => {
+      clearTimeout(mouse.timer);
+      document.documentElement.style.cursor = 'auto';
+      mouse.timer(t);
+    });
+  },
 };
 
-var mouse = mice();
-// to hide cursor when inactive - call: mouse.hide();
+// call: mouse.hide(time/*optional*/);
+// webkit doesn't update cursor if dev tools are opened
